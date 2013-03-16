83d4fede9b3f78f5f9fc0505001e8ebf178818f9
Teijo Laine <teijo@aropupu.fi>
2013-03-17 01:31:43 +0200

    Center clock

--- modules/clock.js
+++ modules/clock.js
@@ -1,6 +1,8 @@
 $(function() {
-  var $container = $('#clock')
-  Bacon.interval(1000).onValue(function() {
+  var $container = $('#clock div')
+  function update() {
     $container.text(moment().format('hh:mm:ss'))
-  })
+  }
+  update()
+  Bacon.interval(1000).onValue(update)
 })
