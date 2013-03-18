d775c59de47023c035db1daf9718f7befa4da594
Teijo Laine <teijo@aropupu.fi>
2013-03-19 00:16:13 +0200

    Resize text on window resize

--- modules/clock.js
+++ modules/clock.js
@@ -3,7 +3,9 @@ $(function() {
   function update() {
     $container.text(moment().format('hh:mm:ss'))
   }
+  function adjust() { $container.textFlood() }
   update()
-  setTimeout(function() { $('#clock').textFlood() }, 1000)
+  setTimeout(adjust, 1000)
+  $(window).asEventStream('resize').throttle(300).onValue(adjust)
   Bacon.interval(1000).onValue(update)
 })
