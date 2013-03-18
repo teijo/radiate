79981a62855df375852919a4e14082fa1729dc58
Teijo Laine <teijo@aropupu.fi>
2013-03-18 23:44:21 +0200

    Replace old flooder with the linked plugin

--- modules/clock.js
+++ modules/clock.js
@@ -1,8 +1,9 @@
 $(function() {
-  var $container = $('#clock div')
+  var $container = $('#clock')
   function update() {
     $container.text(moment().format('hh:mm:ss'))
   }
   update()
+  setTimeout(function() { $('#clock').textFlood() }, 1000)
   Bacon.interval(1000).onValue(update)
 })
